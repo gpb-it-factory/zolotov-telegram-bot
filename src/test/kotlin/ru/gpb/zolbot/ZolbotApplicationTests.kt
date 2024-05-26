edc4efc9780ad53aef989de86e1d3c5062e863fa
Sanf1r <sanfir@yandex.ru>
2024-05-26 18:56:25 +0300

    Add two basic tests, add dependency for mockK

--- src/test/kotlin/ru/gpb/zolbot/ZolbotApplicationTests.kt
+++ src/test/kotlin/ru/gpb/zolbot/ZolbotApplicationTests.kt
@@ -1,13 +1,40 @@
 package ru.gpb.zolbot
 
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.telegram.telegrambots.meta.api.objects.Message
+import org.telegram.telegrambots.meta.api.objects.Update
+import ru.gpb.zolbot.reply.PingCommand
+import ru.gpb.zolbot.reply.ReplyHandler
+import ru.gpb.zolbot.utils.MessageUtils
 
 @SpringBootTest
 class ZolbotApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    private lateinit var replyHandler: ReplyHandler
 
+    @Test
+    fun testStart() {
+        val update = mockk<Update>()
+        every { update.hasMessage() } returns true
+        every { update.message.hasText() } returns true
+        every { update.message.text } returns "/start"
+        every { update.message.chatId } returns 1L
+        assertEquals("Welcome in my GPB telegram bot", replyHandler.handleUpdate(update).text)
+    }
+
+    @Test
+    fun testPing() {
+        val update = mockk<Update>()
+        every { update.hasMessage() } returns true
+        every { update.message.hasText() } returns true
+        every { update.message.text } returns "/ping"
+        every { update.message.chatId } returns 1L
+        assertEquals("pong", replyHandler.handleUpdate(update).text)
+    }
 }
